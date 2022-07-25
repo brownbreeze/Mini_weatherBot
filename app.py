16880b20c48532696aec03d630a95fb3d93107cb
unknown <913breeze@gmail.com>
2022-07-26 01:55:09 +0900

    feat : 타 API 호출
    - 타 API 호출 summary 함수 구현

--- app.py
+++ app.py
@@ -1,17 +1,51 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import json
+import requests
 
-count = 0
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    global count
-    count += 1
-    return jsonify(text='hello world!?',
-                   count = count)
-@app.route('/abuse')
-def abuse_count():
-    global count
-    count += 100
-    return ''
+    return jsonify(text='welcome~ ')
+
+@app.route('/summary')
+def summary():
+    lat = request.args.get('lat')
+    lon = request.args.get('lon')
+
+    api_key = 'CMRJW4WT7V3QA5AOIGPBC'
+    url = 'https://thirdparty-weather-api-v2.droom.workers.dev'
+
+    current_url = '/current'
+    forcast_url = '/forecast/hourly'
+    history_url = '/historical/hourly'
+
+    response = requests.get(url+current_url, params={"lat":lat,"lon":lon, "api_key":api_key})
+
+    print(f'현재')
+    print(response.status_code)
+    print(response.content)
+    greeting = ''
+    print(f'과거')
+    for time in range(-24,0,6):
+        response = requests.get(url + history_url, params={"lat": lat, "lon": lon, "hour_offset":time, "api_key": api_key})
+        print(f'> {time} 전 과거')
+        print(response.status_code)
+        print(response.content)
+    temperature = ''
+    print(f'미래')
+    for time in range(6,50,6):
+        response = requests.get(url + forcast_url, params={"lat": lat, "lon": lon, "hour_offset":time, "api_key": api_key})
+        print(f'> {time} 후 미래')
+        print(response.status_code)
+        print(response.content)
+    heads_up = ''
+    json_object = {
+        "summary" :{
+            "greeting":greeting,
+            "temperature":temperature,
+            "heads-up":heads_up
+        }
+    }
+    return json.dumps(json_object)
     #return jsonify(text='hello world!?', count = 0)
--- app_test.py
+++ app_test.py
@@ -11,31 +11,6 @@ def do_get(client, path):
     pass
 
 def test_sample(client):
-    #client = app.test_client()
-    status_code, body, data = do_get(client, '/')
-    #response = client.get('/')
-    # GET /
-    assert status_code == 200
-    assert 'hello world!?' in body
-
-    oldCount = data['count']
-
     status_code, body, data = do_get(client, '/')
     assert status_code == 200
-
-    newCount = data['count']
-
-    assert newCount == oldCount +  1
-
-def test_abuse(client):
-    status_code, body, data = do_get(client, '/')
-    oldCount = data['count']
-    assert status_code == 200
-
-    status_code, _, _ = do_get(client, 'abuse')
-    assert status_code == 200
-
-    status_code, body, data = do_get(client, '/')
-    newCount = data['count']
-
-    assert newCount == oldCount + 100 + 1
+    assert 'welcome' in body
